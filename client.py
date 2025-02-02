75f8f97146deed7116ee2cf9c38fd574f0954a25
souloist <richardzhaoshen@gmail.com>
2025-02-02 13:39:12 -0500

    Update client to support transaction functionality (begin, rollback, commit)

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 from db import ParrotDB
+from exceptions import KeyNotFound, NoActiveTransactions
 
 
 def main():
@@ -8,7 +9,13 @@ def main():
     Commands:
         set <key> <value>   - Sets the value for the given key
         get <key>           - Returns the value for the given key
+        count <value>       - Returns number of keys with given value
+        delete <key>        - Deletes key
         exit                - Exits the program
+        
+        begin               - Begins a transaction. Supported nested transactions
+        commit              - Commits current transaction
+        rollback            - Rollback current transaction
     """
     print(prompt)
 
@@ -20,11 +27,8 @@ def main():
                 break
 
             parts = command.split()
-            if len(parts) < 2:
-                print("Invalid command.")
-
             action = parts[0].lower()
-            key = parts[1]
+            key = parts[1] if len(parts) > 1 else None
 
             if action == "set":
                 if len(parts) > 3:
@@ -39,12 +43,27 @@ def main():
                     print(f"Get {key}: {value}")
                 else:
                     print(f"No value for {key}.")
-            elif action == "unset":
+            elif action == "delete":
                 if len(parts) > 3:
                     print("Invalid command. Use set <key> <value>.")
                     continue
-                db.unset(key)
-                print(f"Unset {key}")
+                try:
+                    db.delete(key)
+                    print(f"Deleted {key}")
+                except KeyNotFound:
+                    print(f"Key not found")
+            elif action == "begin":
+                db.begin()
+            elif action == "commit":
+                try:
+                    db.commit()
+                except NoActiveTransactions:
+                    print(f"Cannot commit with no active transactions")
+            elif action == "rollback":
+                try:
+                    db.rollback()
+                except NoActiveTransactions:
+                    print(f"Cannot rollback with no active transactions")
             else:
                 print("Unknown command.")
         except KeyboardInterrupt:
